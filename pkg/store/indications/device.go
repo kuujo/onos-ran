ee99e9da86b08f94dafafa4573b4fe73faa3ac54
Jordan Halterman <jordan.halterman@gmail.com>
2020-05-24 02:00:39 -0700

    Update device indications store to send indications to subscribers

--- pkg/store/indications/device.go
+++ pkg/store/indications/device.go
@@ -15,14 +15,18 @@
 package indications
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/onosproject/onos-lib-go/pkg/cluster"
+	"github.com/onosproject/onos-ric/api/sb"
 	"github.com/onosproject/onos-ric/api/store/indications"
 	"github.com/onosproject/onos-ric/pkg/store/device"
 	"github.com/onosproject/onos-ric/pkg/store/mastership"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"io"
 	"sync"
 )
 
@@ -32,7 +36,7 @@ func newDeviceIndicationsStore(deviceKey device.Key, cluster cluster.Cluster, el
 		deviceKey:   deviceKey,
 		cluster:     cluster,
 		election:    election,
-		cache:       make(map[int]*Indication),
+		cache:       make(map[sb.MessageType]*Indication),
 		subscribers: make([]chan<- Event, 0),
 		streams:     make(map[uuid.UUID]indications.IndicationsService_SubscribeServer),
 		recordCh:    make(chan Indication),
@@ -48,7 +52,7 @@ type deviceIndicationsStore struct {
 	deviceKey   device.Key
 	cluster     cluster.Cluster
 	election    mastership.Election
-	cache       map[int]*Indication
+	cache       map[sb.MessageType]*Indication
 	subscribers []chan<- Event
 	state       *mastership.State
 	streams     map[uuid.UUID]indications.IndicationsService_SubscribeServer
@@ -61,29 +65,94 @@ func (s *deviceIndicationsStore) open() error {
 	if err := s.election.Watch(ch); err != nil {
 		return err
 	}
-	go func() {
-		for state := range ch {
-			s.mu.Lock()
-			s.state = &state
-			s.mu.Unlock()
+	go s.watchMastership(ch)
+	go s.processRecords()
+	return nil
+}
+
+func (s *deviceIndicationsStore) processRecords() {
+	for indication := range s.recordCh {
+		s.mu.Lock()
+
+		// Update the local cache
+		s.cache[indication.Hdr.MessageType] = &indication
+
+		// Write the indication event to susbcriber channels
+		for _, subscriber := range s.subscribers {
+			subscriber <- Event{
+				Type:       EventReceived,
+				Indication: indication,
+			}
 		}
-	}()
-	go func() {
-		for indication := range s.recordCh {
-			s.mu.RLock()
-			if s.state != nil {
-				for _, stream := range s.streams {
-					err := stream.Send(&indications.SubscribeResponse{
-						Device:     string(s.deviceKey),
-						Term:       uint64(s.state.Term),
-						Indication: indication.RicIndication,
-					})
-					if err != nil {
-						log.Errorf("Failed to send indication: %s", err)
-					}
+
+		// If this node is the master, send the indication to replicas
+		if s.state != nil && s.state.Master == s.cluster.Node().ID {
+			for _, stream := range s.streams {
+				err := stream.Send(&indications.SubscribeResponse{
+					Device:     string(s.deviceKey),
+					Term:       uint64(s.state.Term),
+					Indication: indication.RicIndication,
+				})
+				if err != nil {
+					log.Errorf("Failed to send indication: %s", err)
 				}
 			}
-			s.mu.RUnlock()
+		}
+		s.mu.Unlock()
+	}
+}
+
+func (s *deviceIndicationsStore) watchMastership(ch <-chan mastership.State) {
+	var cancelFunc context.CancelFunc
+	for state := range ch {
+		s.mu.Lock()
+		s.state = &state
+
+		if state.Master != s.cluster.Node().ID {
+			ctx, cancel := context.WithCancel(context.Background())
+			err := s.subscribeMaster(ctx, state)
+			cancelFunc = cancel
+			if err != nil {
+				log.Errorf("Failed to subscribe to master node %s: %s", state.Master, err)
+			}
+		} else if cancelFunc != nil {
+			cancelFunc()
+		}
+		s.mu.Unlock()
+	}
+}
+
+func (s *deviceIndicationsStore) subscribeMaster(ctx context.Context, mastership mastership.State) error {
+	master := s.cluster.Replica(cluster.ReplicaID(mastership.Master))
+	if master == nil {
+		return fmt.Errorf("cannot find master node %s", mastership.Master)
+	}
+	conn, err := master.Connect()
+	if err != nil {
+		return err
+	}
+	client := indications.NewIndicationsServiceClient(conn)
+	request := &indications.SubscribeRequest{
+		Device: string(s.deviceKey),
+		Term:   uint64(mastership.Term),
+	}
+	stream, err := client.Subscribe(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	// Start a goroutine to receive the stream
+	go func() {
+		for {
+			response, err := stream.Recv()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Errorf("Received subscribe error from master node %s: %s", mastership.Master, err)
+				break
+			} else {
+				s.recordCh <- *New(response.Indication)
+			}
 		}
 	}()
 	return nil
